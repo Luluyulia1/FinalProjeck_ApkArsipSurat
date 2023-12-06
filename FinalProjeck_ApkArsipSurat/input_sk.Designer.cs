62000f9b0fac4c6ebf356b551925a3e3d9850e16
masnaarinaputri <masnaarina@gmail.com>
2023-12-06 15:58:13 +0700

    Form Input Surat Keluar selesai

---
+++ FinalProjeck_ApkArsipSurat/input_sk.Designer.cs
@@ -0,0 +1,368 @@
+﻿
+namespace FinalProjeck_ApkArsipSurat
+{
+    partial class input_sk
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtNoSurat = new System.Windows.Forms.TextBox();
+            this.txtKodeSurat = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtPerihal = new System.Windows.Forms.TextBox();
+            this.txtTujuan = new System.Windows.Forms.TextBox();
+            this.txtIsi = new System.Windows.Forms.TextBox();
+            this.btnCetak = new System.Windows.Forms.Button();
+            this.btnUbah = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.btnBatal = new System.Windows.Forms.Button();
+            this.btnSimpan = new System.Windows.Forms.Button();
+            this.btnBaru = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtCaridata = new System.Windows.Forms.TextBox();
+            this.btnCari = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnKembali = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(253, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "INPUT SURAT";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Id";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kode Surat";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "No Surat";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 242);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Tanggal";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(40, 298);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Perihal";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(40, 362);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(57, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Tujuan";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(40, 422);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(25, 20);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Isi";
+            // 
+            // txtNoSurat
+            // 
+            this.txtNoSurat.Location = new System.Drawing.Point(152, 179);
+            this.txtNoSurat.Name = "txtNoSurat";
+            this.txtNoSurat.Size = new System.Drawing.Size(393, 26);
+            this.txtNoSurat.TabIndex = 8;
+            // 
+            // txtKodeSurat
+            // 
+            this.txtKodeSurat.Location = new System.Drawing.Point(152, 117);
+            this.txtKodeSurat.Name = "txtKodeSurat";
+            this.txtKodeSurat.Size = new System.Drawing.Size(393, 26);
+            this.txtKodeSurat.TabIndex = 9;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(152, 74);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(393, 26);
+            this.txtId.TabIndex = 10;
+            // 
+            // txtPerihal
+            // 
+            this.txtPerihal.Location = new System.Drawing.Point(152, 295);
+            this.txtPerihal.Name = "txtPerihal";
+            this.txtPerihal.Size = new System.Drawing.Size(393, 26);
+            this.txtPerihal.TabIndex = 11;
+            // 
+            // txtTujuan
+            // 
+            this.txtTujuan.Location = new System.Drawing.Point(152, 362);
+            this.txtTujuan.Name = "txtTujuan";
+            this.txtTujuan.Size = new System.Drawing.Size(393, 26);
+            this.txtTujuan.TabIndex = 12;
+            // 
+            // txtIsi
+            // 
+            this.txtIsi.Location = new System.Drawing.Point(152, 419);
+            this.txtIsi.Name = "txtIsi";
+            this.txtIsi.Size = new System.Drawing.Size(393, 26);
+            this.txtIsi.TabIndex = 13;
+            // 
+            // btnCetak
+            // 
+            this.btnCetak.Location = new System.Drawing.Point(499, 471);
+            this.btnCetak.Name = "btnCetak";
+            this.btnCetak.Size = new System.Drawing.Size(75, 36);
+            this.btnCetak.TabIndex = 14;
+            this.btnCetak.Text = "Cetak";
+            this.btnCetak.UseVisualStyleBackColor = true;
+            // 
+            // btnUbah
+            // 
+            this.btnUbah.Location = new System.Drawing.Point(407, 471);
+            this.btnUbah.Name = "btnUbah";
+            this.btnUbah.Size = new System.Drawing.Size(75, 36);
+            this.btnUbah.TabIndex = 15;
+            this.btnUbah.Text = "Ubah";
+            this.btnUbah.UseVisualStyleBackColor = true;
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Location = new System.Drawing.Point(317, 471);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(75, 36);
+            this.btnHapus.TabIndex = 16;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            // 
+            // btnBatal
+            // 
+            this.btnBatal.Location = new System.Drawing.Point(227, 471);
+            this.btnBatal.Name = "btnBatal";
+            this.btnBatal.Size = new System.Drawing.Size(75, 36);
+            this.btnBatal.TabIndex = 17;
+            this.btnBatal.Text = "Batal";
+            this.btnBatal.UseVisualStyleBackColor = true;
+            // 
+            // btnSimpan
+            // 
+            this.btnSimpan.Location = new System.Drawing.Point(137, 471);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(75, 36);
+            this.btnSimpan.TabIndex = 18;
+            this.btnSimpan.Text = "Simpan";
+            this.btnSimpan.UseVisualStyleBackColor = true;
+            this.btnSimpan.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // btnBaru
+            // 
+            this.btnBaru.Location = new System.Drawing.Point(44, 471);
+            this.btnBaru.Name = "btnBaru";
+            this.btnBaru.Size = new System.Drawing.Size(75, 36);
+            this.btnBaru.TabIndex = 19;
+            this.btnBaru.Text = "Baru";
+            this.btnBaru.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(152, 242);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(393, 26);
+            this.numericUpDown1.TabIndex = 20;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(731, 30);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(197, 20);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "DATA SURAT KELUAR";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(607, 77);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(192, 20);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Cari data berdasarkan Id :";
+            // 
+            // txtCaridata
+            // 
+            this.txtCaridata.Location = new System.Drawing.Point(805, 71);
+            this.txtCaridata.Name = "txtCaridata";
+            this.txtCaridata.Size = new System.Drawing.Size(234, 26);
+            this.txtCaridata.TabIndex = 23;
+            // 
+            // btnCari
+            // 
+            this.btnCari.Location = new System.Drawing.Point(1068, 73);
+            this.btnCari.Name = "btnCari";
+            this.btnCari.Size = new System.Drawing.Size(75, 27);
+            this.btnCari.TabIndex = 24;
+            this.btnCari.Text = "Cari";
+            this.btnCari.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(611, 123);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(532, 305);
+            this.dataGridView1.TabIndex = 25;
+            // 
+            // btnKembali
+            // 
+            this.btnKembali.Location = new System.Drawing.Point(1068, 455);
+            this.btnKembali.Name = "btnKembali";
+            this.btnKembali.Size = new System.Drawing.Size(75, 36);
+            this.btnKembali.TabIndex = 26;
+            this.btnKembali.Text = "Kembali";
+            this.btnKembali.UseVisualStyleBackColor = true;
+            // 
+            // input_sk
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(1181, 545);
+            this.Controls.Add(this.btnKembali);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnCari);
+            this.Controls.Add(this.txtCaridata);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.btnBaru);
+            this.Controls.Add(this.btnSimpan);
+            this.Controls.Add(this.btnBatal);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnUbah);
+            this.Controls.Add(this.btnCetak);
+            this.Controls.Add(this.txtIsi);
+            this.Controls.Add(this.txtTujuan);
+            this.Controls.Add(this.txtPerihal);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.txtKodeSurat);
+            this.Controls.Add(this.txtNoSurat);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "input_sk";
+            this.Text = "input_sk";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtNoSurat;
+        private System.Windows.Forms.TextBox txtKodeSurat;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtPerihal;
+        private System.Windows.Forms.TextBox txtTujuan;
+        private System.Windows.Forms.TextBox txtIsi;
+        private System.Windows.Forms.Button btnCetak;
+        private System.Windows.Forms.Button btnUbah;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.Button btnBatal;
+        private System.Windows.Forms.Button btnSimpan;
+        private System.Windows.Forms.Button btnBaru;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtCaridata;
+        private System.Windows.Forms.Button btnCari;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnKembali;
+    }
+}
---
+++ FinalProjeck_ApkArsipSurat/input_sk.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProjeck_ApkArsipSurat
+{
+    public partial class input_sk : Form
+    {
+        public input_sk()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
