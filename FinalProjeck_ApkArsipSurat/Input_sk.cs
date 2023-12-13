ee94013cbee96f49d767c1e8224c4d59202b1131
masnaarinaputri <masnaarina@gmail.com>
2023-12-13 09:40:09 +0700

    Input SK selesai

---
+++ FinalProjeck_ApkArsipSurat/Input_sk.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProjeck_ApkArsipSurat
+{
+    public partial class Input_sk : Form
+    {
+        public Input_sk()
+        {
+            InitializeComponent();
+        }
+    }
+}
