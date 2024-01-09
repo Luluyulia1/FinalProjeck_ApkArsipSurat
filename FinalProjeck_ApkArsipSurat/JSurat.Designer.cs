bebb7beee8f1820272573588d8cd0645a9ec2390
Lulu Yuliastuti <luluyulia15@gmail.com>
2024-01-09 21:19:27 +0700

    Jenis_Surat Selesai

---
+++ FinalProjeck_ApkArsipSurat/JSurat.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace FinalProjeck_ApkArsipSurat
+{
+    partial class JSurat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgJS = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnUbah = new System.Windows.Forms.Button();
+            this.btnSimpan = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.btnTambah = new System.Windows.Forms.Button();
+            this.txtJS = new System.Windows.Forms.TextBox();
+            this.txtKodeJS = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dgJS)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgJS
+            // 
+            this.dgJS.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgJS.Location = new System.Drawing.Point(533, 142);
+            this.dgJS.Margin = new System.Windows.Forms.Padding(4);
+            this.dgJS.Name = "dgJS";
+            this.dgJS.RowHeadersWidth = 51;
+            this.dgJS.Size = new System.Drawing.Size(536, 314);
+            this.dgJS.TabIndex = 110;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Book Antiqua", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(673, 22);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Jenis Surat";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(412, 339);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(88, 37);
+            this.btnClear.TabIndex = 115;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(19, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 47);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Arsip Surat";
+            // 
+            // btnUbah
+            // 
+            this.btnUbah.Location = new System.Drawing.Point(221, 339);
+            this.btnUbah.Margin = new System.Windows.Forms.Padding(4);
+            this.btnUbah.Name = "btnUbah";
+            this.btnUbah.Size = new System.Drawing.Size(89, 37);
+            this.btnUbah.TabIndex = 114;
+            this.btnUbah.Text = "Ubah";
+            this.btnUbah.UseVisualStyleBackColor = true;
+            // 
+            // btnSimpan
+            // 
+            this.btnSimpan.Location = new System.Drawing.Point(132, 339);
+            this.btnSimpan.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(81, 37);
+            this.btnSimpan.TabIndex = 112;
+            this.btnSimpan.Text = "Simpan";
+            this.btnSimpan.UseVisualStyleBackColor = true;
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Location = new System.Drawing.Point(319, 339);
+            this.btnHapus.Margin = new System.Windows.Forms.Padding(4);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(85, 37);
+            this.btnHapus.TabIndex = 113;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            // 
+            // btnTambah
+            // 
+            this.btnTambah.Location = new System.Drawing.Point(43, 339);
+            this.btnTambah.Margin = new System.Windows.Forms.Padding(4);
+            this.btnTambah.Name = "btnTambah";
+            this.btnTambah.Size = new System.Drawing.Size(81, 37);
+            this.btnTambah.TabIndex = 111;
+            this.btnTambah.Text = "Tambah";
+            this.btnTambah.UseVisualStyleBackColor = true;
+            // 
+            // txtJS
+            // 
+            this.txtJS.Location = new System.Drawing.Point(221, 217);
+            this.txtJS.Margin = new System.Windows.Forms.Padding(4);
+            this.txtJS.Name = "txtJS";
+            this.txtJS.Size = new System.Drawing.Size(284, 22);
+            this.txtJS.TabIndex = 109;
+            // 
+            // txtKodeJS
+            // 
+            this.txtKodeJS.Location = new System.Drawing.Point(221, 175);
+            this.txtKodeJS.Margin = new System.Windows.Forms.Padding(4);
+            this.txtKodeJS.Name = "txtKodeJS";
+            this.txtKodeJS.Size = new System.Drawing.Size(284, 22);
+            this.txtKodeJS.TabIndex = 108;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(60, 222);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 20);
+            this.label4.TabIndex = 107;
+            this.label4.Text = "Jenis Surat";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(60, 180);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 20);
+            this.label3.TabIndex = 106;
+            this.label3.Text = "Kode Jenis Surat";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSlateGray;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1151, 75);
+            this.panel1.TabIndex = 105;
+            // 
+            // JSurat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1151, 492);
+            this.Controls.Add(this.dgJS);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnUbah);
+            this.Controls.Add(this.btnSimpan);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnTambah);
+            this.Controls.Add(this.txtJS);
+            this.Controls.Add(this.txtKodeJS);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Name = "JSurat";
+            this.Text = "JSurat";
+            ((System.ComponentModel.ISupportInitialize)(this.dgJS)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgJS;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnUbah;
+        private System.Windows.Forms.Button btnSimpan;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.Button btnTambah;
+        private System.Windows.Forms.TextBox txtJS;
+        private System.Windows.Forms.TextBox txtKodeJS;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
